b0440e9de479fbac0ca96cb5832d4b779f79e32d
supriyaarora12 <supriya.arora.hp@gmail.com>
2026-01-12 14:03:46 +0530

    apifix

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,10 +54,11 @@ export function trackButtonClick(buttonId: string): void {
         headers: {
           'Content-Type': 'application/json',
           Accept: 'application/json',
-          'X-API-Key': 'gAAAAABpVNqy0Gs3i5WxaEF6vk8slMC9IvWoR7S8iMMKWMXeLT49fcwpiBPWqV_GpGJYPKZb-oqZhpbHCpIrJXOjquwiFMPeGj9oy3i5rAUiM01P5QxXdxb-l30QN4MrvPWHiTSRSbIW',
+          'X-API-Key': 'gAAAAABpZI7sMrya9JkkulQukUR0ZkRnpja71e0b6gdGgfbLVH7UQLu1W_Sv1hy0lFvRJpqUZyUTVdxLpsF3r5IZGk3Q9g_3ffFydo-BoOE1B7fLIWSHNTrK3hN1czhmpkLedf1MfycW',
         },
         body: JSON.stringify({
-          client_id: '983f1c31-e5b3-4b14-bff4-ae370010bd82',
+          client_id: '1435480d-deae-4596-afb7-60f96763352c',
+          project_id: 'cea54c03-12ed-4a58-aae8-70d867e4ff6a',
           button_id: buttonId,
           count: 1,
           ip_address: clientIP,
